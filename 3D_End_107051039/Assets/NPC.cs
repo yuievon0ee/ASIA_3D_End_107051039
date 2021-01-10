30800343c5946b75c204c33149b22c02c912d351
yuievon0ee <71679885+yuievon0ee@users.noreply.github.com>
2021-01-10 11:49:25 +0800

    對話

---
+++ 3D_End_107051039/Assets/NPC.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="NPC資料",menuName ="Yui/NPC資料")]
+public class NPC : ScriptableObject
+{
+    [Header("蝙蝠對話1"),TextArea(1,5)]
+    public string dialougA;
+    [Header("蝙蝠對話2"), TextArea(1, 5)]
+    public string dialougB;
+    [Header("蝙蝠對話3"), TextArea(1, 5)]
+    public string dialougC;
+    [Header("任務項目需求數量")]
+    public int count;
+    [Header("已取得項目數量")]
+    public int countCurrent;
+
+}
---
+++ 3D_End_107051039/Assets/NPCTWO.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public class NPCTWO : MonoBehaviour
+{
+    [Header("NPC 資料")]
+    public NPC data;
+    [Header("畫布")]
+    public GameObject dialog;
+    [Header("內容")]
+    public Text textContent;
+
+    public bool playerInArea;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name== "ThirdPerson")
+        {
+            playerInArea = true;
+            Dialog();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.name== "ThirdPerson")
+        {
+            playerInArea = false;
+        }
+    }
+
+    private void Dialog()
+    {
+        for(int i=0;i<data.dialougA.Length;i++)
+        {
+            print(data.dialougA[i]);
+        }
+    }
+}
