f81049dd2d8fc7b4cdcd81f54123f161eaa0120e
yuievon0ee <71679885+yuievon0ee@users.noreply.github.com>
2021-01-16 13:26:56 +0800

    怪物AI-2

---
+++ 3D_End_107051039/Assets/Enemy.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine.AI;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("移動速度"), Range(0, 50)]
+    public float speed = 3;
+    [Header("停止距離"), Range(0, 50)]
+    public float stopDistance = 2.5f;
+    [Header("攻擊冷卻時間"), Range(0, 50)]
+    public float cd = 2f;
+    [Header("攻擊中心點")]
+    public Transform atkPoint;
+    [Header("攻擊長度"), Range(0f, 5f)]
+    public float atkLength;
+
+    private Transform player;
+    private NavMeshAgent nav;
+    private Animator ani;
+
+    private float timer;
+
+    private void Awake()
+    {
+        nav = GetComponent<NavMeshAgent>();
+        ani = GetComponent<Animator>();
+        player = GameObject.Find("ThirdPerson").transform;
+
+        nav.speed = speed;
+        nav.stoppingDistance = stopDistance;
+    }
+
+    private void Update()
+    {
+        Track();
+        Attack();
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(atkPoint.position, atkPoint.forward * atkLength);
+    }
+
+    private RaycastHit hit;
+
+    private void Attack()
+    {
+        if (nav.remainingDistance < stopDistance)
+        {
+            timer += Time.deltaTime;
+            Vector3 pos = player.position;
+            pos.y = transform.position.y;
+            transform.LookAt(pos);
+
+            if(timer>=cd)
+            {
+                ani.SetTrigger("攻擊觸發");
+                timer = 0;
+                if(Physics.Raycast(atkPoint.position, atkPoint.forward,out hit, atkLength, 1 << 8))
+                {
+                    hit.collider.GetComponent<Player>().Damage();
+                }
+            }
+            
+        }
+    }
+
+    private void Track()
+    {
+
+        nav.SetDestination(player.position);
+
+        ani.SetBool("跑步開關", nav.remainingDistance > stopDistance);
+    }
+}
---
+++ 3D_End_107051039/Assets/Player.cs
@@ -0,0 +1,10 @@
+﻿
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public void Damage()
+    {
+        print("受傷");
+    }
+}
